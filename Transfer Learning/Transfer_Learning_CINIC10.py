e5cb0b67005bb1406a677d59bca770dcc96c82cf
H M Dipu Kabir <dipuk0506@gmail.com>
2020-09-08 10:09:52 +1000

    Add files via upload

---
+++ Transfer Learning/Transfer_Learning_CINIC10.py
@@ -0,0 +1,326 @@
+'''
+We write this code with the help of PyTorch demo:
+    https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
+    
+The Dataset is downloaded from https://www.kaggle.com/mengcius/cinic10
+
+
+Effects:
+        transforms.Resize((272,272)),
+        transforms.RandomRotation(15,),
+        transforms.RandomCrop(256),
+        transforms.RandomHorizontalFlip(),
+
+wide_resnet101_2 Spinal FC gives 93.60% test accuracy
+
+'''
+
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+
+
+
+plt.ion()   # interactive mode
+
+# Data augmentation and normalization for training
+# Just normalization for validation
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.Resize((456,456)),
+        transforms.RandomRotation(15,),
+        transforms.RandomCrop(448),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.47889522, 0.47227842, 0.43047404], std=[0.24205776, 0.23828046, 0.25874835])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize((448,448)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.47889522, 0.47227842, 0.43047404], std=[0.24205776, 0.23828046, 0.25874835])
+    ]),
+    'test': transforms.Compose([
+        transforms.Resize((448,448)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.47889522, 0.47227842, 0.43047404], std=[0.24205776, 0.23828046, 0.25874835])
+    ]),
+}
+
+data_dir = 'data/cinic10'
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'val', 'test']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=9,
+                                             shuffle=True, num_workers=0)
+              for x in ['train', 'val', 'test']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val', 'test']}
+class_names = image_datasets['train'].classes
+
+device = 'cuda'#torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+
+# Get a batch of training data
+inputs, classes = next(iter(dataloaders['train']))
+
+# Make a grid from batch
+out = torchvision.utils.make_grid(inputs)
+
+imshow(out)#, title=[class_names[x] for x in classes])
+
+
+#%%
+
+# model_ft = models.vgg19_bn(pretrained=True)
+# num_ftrs = model_ft.classifier[0].in_features
+
+model_ft = models.wide_resnet101_2(pretrained=True)
+num_ftrs = model_ft.fc.in_features
+
+half_in_size = round(num_ftrs/2)
+layer_width_vgg = 1024 #Small for Resnet, large for VGG
+layer_width_res = 20 #Small for Resnet, large for VGG
+Num_class=10
+
+class SpinalNet_ResNet(nn.Module):
+    def __init__(self):
+        super(SpinalNet_ResNet, self).__init__()
+        
+        self.fc_spinal_layer1 = nn.Sequential(
+            #nn.Dropout(p = 0.5), 
+            nn.Linear(half_in_size, layer_width_res),
+            #nn.BatchNorm1d(layer_width), 
+            nn.ReLU(inplace=True),)
+        self.fc_spinal_layer2 = nn.Sequential(
+            #nn.Dropout(p = 0.5), 
+            nn.Linear(half_in_size+layer_width_res, layer_width_res),
+            #nn.BatchNorm1d(layer_width), 
+            nn.ReLU(inplace=True),)
+        self.fc_spinal_layer3 = nn.Sequential(
+            #nn.Dropout(p = 0.5), 
+            nn.Linear(half_in_size+layer_width_res, layer_width_res),
+            #nn.BatchNorm1d(layer_width), 
+            nn.ReLU(inplace=True),)
+        self.fc_spinal_layer4 = nn.Sequential(
+            #nn.Dropout(p = 0.5), 
+            nn.Linear(half_in_size+layer_width_res, layer_width_res),
+            #nn.BatchNorm1d(layer_width), 
+            nn.ReLU(inplace=True),)
+        self.fc_out = nn.Sequential(
+            #nn.Dropout(p = 0.5), 
+            nn.Linear(layer_width_res*4, Num_class),)
+        
+    def forward(self, x):
+        x1 = self.fc_spinal_layer1(x[:, 0:half_in_size])
+        x2 = self.fc_spinal_layer2(torch.cat([ x[:,half_in_size:2*half_in_size], x1], dim=1))
+        x3 = self.fc_spinal_layer3(torch.cat([ x[:,0:half_in_size], x2], dim=1))
+        x4 = self.fc_spinal_layer4(torch.cat([ x[:,half_in_size:2*half_in_size], x3], dim=1))
+        
+        
+        x = torch.cat([x1, x2], dim=1)
+        x = torch.cat([x, x3], dim=1)
+        x = torch.cat([x, x4], dim=1)
+
+        
+        x = self.fc_out(x)
+        return x
+    
+class SpinalNet_VGG(nn.Module):
+    def __init__(self):
+        super(SpinalNet_VGG, self).__init__()
+        
+        self.fc_spinal_layer1 = nn.Sequential(
+            nn.Dropout(p = 0.5), nn.Linear(half_in_size, layer_width_vgg),
+            nn.BatchNorm1d(layer_width_vgg), nn.ReLU(inplace=True),)
+        self.fc_spinal_layer2 = nn.Sequential(
+            nn.Dropout(p = 0.5), 
+            nn.Linear(half_in_size+layer_width_vgg, layer_width_vgg),
+            nn.BatchNorm1d(layer_width_vgg), 
+            nn.ReLU(inplace=True),)
+        self.fc_spinal_layer3 = nn.Sequential(
+            nn.Dropout(p = 0.5), 
+            nn.Linear(half_in_size+layer_width_vgg, layer_width_vgg),
+            nn.BatchNorm1d(layer_width_vgg), 
+            nn.ReLU(inplace=True),)
+        self.fc_spinal_layer4 = nn.Sequential(
+            nn.Dropout(p = 0.5), 
+            nn.Linear(half_in_size+layer_width_vgg, layer_width_vgg),
+            nn.BatchNorm1d(layer_width_vgg), 
+            nn.ReLU(inplace=True),)
+        self.fc_out = nn.Sequential(
+            nn.Dropout(p = 0.5), 
+            nn.Linear(layer_width_vgg*4, Num_class),)        
+
+    def forward(self, x):
+        x1 = self.fc_spinal_layer1(x[:, 0:half_in_size])
+        x2 = self.fc_spinal_layer2(torch.cat([ x[:,half_in_size:2*half_in_size], x1], dim=1))
+        x3 = self.fc_spinal_layer3(torch.cat([ x[:,0:half_in_size], x2], dim=1))
+        x4 = self.fc_spinal_layer4(torch.cat([ x[:,half_in_size:2*half_in_size], x3], dim=1))
+        
+        
+        x = torch.cat([x1, x2], dim=1)
+        x = torch.cat([x, x3], dim=1)
+        x = torch.cat([x, x4], dim=1)
+
+        
+        x = self.fc_out(x)
+        return x
+
+
+
+VGG_fc = nn.Sequential(
+            nn.Linear(512, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, Num_class)
+        )
+
+
+'''
+Changing the fully connected layer to SpinalNet or VGG or ResNet
+'''
+
+#model_ft.fc = nn.Linear(num_ftrs, Num_class)
+#model_ft.fc = SpinalNet_VGG #SpinalNet_ResNet() #SpinalNet_VGG
+model_ft.fc = SpinalNet_ResNet()
+#%%
+
+
+
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    test_token=0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val', 'test']:
+             
+            
+            '''
+            Test when a better validation result is found
+            '''
+            if test_token ==0 and phase == 'test' and epoch>8:
+                continue
+            test_token =0
+            
+            
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+                test_token =1
+                time_elapsed = time.time() - since
+                print('Time from start {:.0f}m {:.0f}s'.format(
+                    time_elapsed // 60, time_elapsed % 60))
+
+
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+        
+
+model_ft = model_ft.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# Observe that all parameters are being optimized
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.01, momentum=0.9)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                       num_epochs=20)
+
+
+
+# Observe that all parameters are being optimized
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                       num_epochs=20)
